74364c165f4922704c23ae46b6c1d1eb33ac0d26
daachan <daichan2416@gmail.com>
2024-11-05 14:48:54 +0900

    first commit for create repository

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function($){
+    $(window).on('load scroll', function(){
+        
+        var horizonAnime = 'horizon-line-anime';
+        var horizonLine = $('.horizon-line-anime');
+        var verticalAnime = 'vertical-line-anime';
+        var verticalLine = $('.vertical-line-anime');
+        var fadeInAnime = 'fadeIn'
+        var fadeIn = $('.fadeIn');
+
+        var scrollPos = $(window).scrollTop();
+        var wh = $(window).height();
+        
+        horizonLine.each(function(){
+            var boxOffset = $(this).offset().top;
+
+            if (scrollPos > boxOffset - wh + 100){
+                $(this).removeClass(horizonAnime);
+            }
+        });
+
+        verticalLine.each(function(){
+            var boxOffset = $(this).offset().top;
+
+            if (scrollPos > boxOffset - wh + 100){
+                $(this).removeClass(verticalAnime);
+            }
+        });
+        
+        fadeIn.each(function(){
+            var boxOffset = $(this).offset().top;
+
+            if (scrollPos > boxOffset - wh + 200){
+                $(this).removeClass(fadeInAnime);
+            }
+        });
+    });
+});
